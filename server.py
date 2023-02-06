33e898e811e9462ee7d66764011292ead01486c6
youswim <youswim96@gmail.com>
2023-02-06 23:18:05 +0900

    refactor(server.py): 사용하지 않는 코드 삭제

--- server.py
+++ server.py
@@ -19,18 +19,6 @@ app = Flask(__name__)
 @app.route('/')
 def SetUp():
     return "Hello server!"
-    
-@app.route("/led1/on")
-def led_1_on():
-    send_message("1")
-    print("led1 on")
-    return "LED 1 ON"
-
-@app.route("/led2/on")
-def led_2_on():
-    send_message("2")
-    print("led2 on")
-    return "LED 2 ON"
 
 @app.route("/ledtime")
 def get_led_time():
@@ -65,17 +53,6 @@ def receive_message_from_mq():
         channel.start_consuming()
     except KeyboardInterrupt:
         pass
-
-def send_message(light_number):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=g_host_name))
-    channel = connection.channel()
-
-    channel.queue_declare(queue=g_change_request_queue, arguments={'x-message-ttl' : int(1000)})
-
-    channel.basic_publish(exchange='', routing_key=g_change_request_queue, body=light_number)
-    print(f"Sent message.\n{light_number}")
-    connection.close()
-
 
 try:
     if __name__== "__main__":
